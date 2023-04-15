3adb771055e58bf78d8d2792cbb9f63cb1df3260
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2023-04-15 16:00:12 +0200

    updated images

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,12 +2,12 @@ import Head from 'next/head'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillLinkedin, AiFillGithub} from 'react-icons/ai'
 import Image from "next/legacy/image"
-import anna from prefix + "/public/anna-bewerbungsbild.jpg"
+import anna from "/personal-website/../../public/anna-bewerbungsbild.jpg"
 import design from "../../public/design.png"
-import erasmus from prefix + "/public/erasmus_viz.png"
+import erasmus from "/personal-website/../../public/erasmus_viz.png"
 import code from "../../public/code.png"
-import mia from prefix + "/public/team-duckling.jpg"
-import annalogie from prefix + "/public/logo.png"
+import mia from "/personal-website/../../public/team-duckling.jpg"
+import annalogie from "/personal-website/../../public/logo.png"
 import {useState} from "react";
 export default function Home() {
   const [darkMode, setDarkMode] = useState(false);

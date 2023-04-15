c0a8a4bc087efa16693dc496e140c3e08ff9ae7b
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2023-04-15 16:35:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
 module.exports = nextConfig
 
 module.exports = {
-  images: {
-    unoptimized: true,
-  },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH
 }
